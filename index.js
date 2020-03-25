465b0bf390defc6e25638e843fbffc7f69487a0a
Luiz Fernando <fernandoaff1@gmail.com>
2020-03-25 15:36:53 -0300

    Add GridStack

---
+++ index.js
@@ -0,0 +1,16 @@
+var StaticServer = require('static-server');
+var server = new StaticServer({
+  rootPath: '.',            // required, the root of the server file tree
+  port: 1337,               // required, the port to listen
+  name: 'my-http-server',   // optional, will set "X-Powered-by" HTTP header
+  cors: '*',                // optional, defaults to undefined
+  followSymlink: true,      // optional, defaults to a 404 error
+  templates: {
+    index: 'foo.html',      // optional, defaults to 'index.html'
+    notFound: '404.html'    // optional, defaults to undefined
+  }
+});
+ 
+server.start(function () {
+  console.log('Server listening to', server.port);
+});
