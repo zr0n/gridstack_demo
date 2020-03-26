0dd8c83aaf55b739637a4f9732bd651e065b7175
Luiz Fernando <fernandoaff1@gmail.com>
2020-03-26 08:44:22 -0300

    Serializing Data

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-GridStack.init();
+function saveData() {
+    var items = [];
+
+    $('.grid-stack-item.ui-draggable').each(function () {
+        var $this = $(this);
+        items.push({
+            x: $this.attr('data-gs-x'),
+            y: $this.attr('data-gs-y'),
+            w: $this.attr('data-gs-width'),
+            h: $this.attr('data-gs-height'),
+            content: $('.grid-stack-item-content', $this).html()
+        });
+    });
+
+    alert(JSON.stringify(items));
+}
+
+document.addEventListener('keypress', (keyevent) => keyevent.key === 's' && saveData());
